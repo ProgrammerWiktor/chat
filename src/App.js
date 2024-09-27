d9ad17b08dc9545077e34577b9ec416a988c8e0a
Wiktor Opala <wiktor75300@gmail.com>
2024-09-27 13:21:25 +0200

    add background for app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      Hello World
+    <div className="container">
+      <div className='app'>
+        
+      </div>
     </div>
   );
 }
