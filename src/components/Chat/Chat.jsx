b2c0cf6dee684de386af82946e5dd3fa34794e6d
Wiktor Opala <wiktor75300@gmail.com>
2024-09-29 11:21:20 +0200

    add showing messages template

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Chat.module.css";
 
 const Chat = () => {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.messages}>
-        <span>Chats</span>
-        <span>Chats</span>
-        <span>Chats</span>
+      <div className={styles.yourMessage}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </div>
+      <div className={styles.message}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum.
+      </div>
+      <div className={styles.yourMessage}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum. In sed lectus nisi.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Aenean et justo dictum nunc condimentum aliquet ut nec
+        massa.
+      </div>
+      <div className={styles.message}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum.
+      </div>
+      <div className={styles.yourMessage}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div className={styles.message}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum.
+      </div>
+      <div className={styles.yourMessage}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum. In sed lectus nisi.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Aenean et justo dictum nunc condimentum aliquet ut nec
+        massa.
+      </div>
+      <div className={styles.message}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        bibendum quam nec sapien malesuada condimentum.
+      </div>
+      <div ref={messagesEndRef} />
     </div>
   );
 };
