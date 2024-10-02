dc38adedec4b73ca27f029f152c8fb6138f23a35
Wiktor Opala <wiktor75300@gmail.com>
2024-10-02 18:55:45 +0200

    add showing username

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 const Profile = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
 
   const handleLogout = async () => {
     try {
@@ -16,6 +18,30 @@ const Profile = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const user = auth.currentUser;
+
+        if (user) {
+          const uid = user.uid;
+
+          const userDocRef = doc(db, "users", uid);
+
+          const userDoc = await getDoc(userDocRef);
+
+          if (userDoc.exists()) {
+            setUsername(userDoc.data().username);
+          }
+        }
+      } catch {
+        alert("Nie udało się pobrać nazwy użytkownika");
+      }
+    };
+
+    fetchUsername();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -24,7 +50,7 @@ const Profile = () => {
           alt="Zdjęcie profilowe"
           className={styles.profileImage}
         />
-        <p className={styles.name}>Jan Kowalski</p>
+        <p className={styles.name}>{username}</p>
       </div>
       <button className={styles.logout} onClick={handleLogout}>
         <img
